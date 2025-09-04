a1f9608d20ef7f6f4e091b9f1b4d01cc61643cbc
AriB2003 <ari.bobesh@gmail.com>
2025-09-04 19:46:47 -0400

    Add final breakpoints to newton

--- newton_solver.m
+++ newton_solver.m
@@ -3,14 +3,19 @@ function x = newton_solver(fun,x0)
 %NEWTON_SOLVER Newton's method root solver
 %   fun: callable that returns [f,dfdx]
 %   x0: starting x guess
+%   x: zero if found or closest iteration
     a_thresh = 10^-14;
     b_thresh = 10^-14;
     last_xn = x0-2*a_thresh;
     xn = x0;
     [fxn,fdxn] = fun(xn);
-    while abs(xn-last_xn)>a_thresh && abs(fxn)>b_thresh
+    while abs(xn-last_xn)>a_thresh && abs(fxn)>b_thresh && fdxn~=0
         last_xn = xn;
-        xn = xn - fxn/fdxn;
+        diff = fxn/fdxn;
+        if diff>10^6
+            break
+        end
+        xn = xn - diff;
         [fxn,fdxn] = fun(xn);
     end
     x = xn;
