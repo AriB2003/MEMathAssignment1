5c9a6db6f786eec7e8a03219b57f71e57b7f05c7
AriB2003 <ari.bobesh@gmail.com>
2025-09-12 16:13:11 -0400

    Add some eggcellent code

---
+++ egg_wrappers.m
@@ -0,0 +1,27 @@
+%set the oval hyper-parameters
+egg_params = struct();
+egg_params.a = 3; egg_params.b = 2; egg_params.c = .15;
+%specify the position and orientation of the egg
+x0 = 5; y0 = 5; theta = pi/6;
+
+% %compute the perimeter of the egg
+% [V_list, G_list] = egg_func(linspace(0,1,100),x0,y0,theta,egg_params);
+% %plot the perimeter of the egg
+% plot(V_list(1,:),V_list(2,:),'k');
+% hold on
+% [x_bound, y_bound] = compute_bounding_box(x0,y0,theta,egg_params)
+% rectangle(Position=[x_bound(1),y_bound(1),x_bound(2)-x_bound(1),y_bound(2)-y_bound(1)],EdgeColor="c")
+% 
+% t=0:0.05:10;
+% [x,y,theta] = egg_trajectory01(t);
+% figure;
+% plot(x,y,".")
+
+[t_g, t_w]=collision_func(@egg_trajectory01, egg_params, -10, 40)
+
+%Example parabolic trajectory
+function [x0,y0,theta] = egg_trajectory01(t)
+    x0 = 7*t + 8;
+    y0 = -6*t.^2 + 20*t + 6;
+    theta = 5*t;
+end
