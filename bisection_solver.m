bda165fcab1a2d89179f9927f87d9c25129fa622
wyoung1810 <ukjoma@gmail.com>
2025-09-18 16:30:20 -0400

    Added comments

--- bisection_solver.m
+++ bisection_solver.m
@@ -4,13 +4,22 @@ function [x, success] = bisection_solver(fun,x_left,x_right)
     %x_left: starting left bound
     %x_right: starting right bound
     %x: zero if found
-
+    %success: A flag that is True if the solver converges, False otherwise
+    
     x_mid = x_left;
+
+    % Set thresholds for closeness to root
     a_thresh = 10^-14;
     b_thresh = 10^-14;
     success = true;
+
+    
     while abs(x_left - x_right) > a_thresh && abs(fun(x_mid)) > b_thresh
+       % Find the midpoint
        x_mid = (x_left + x_right)/2;
+
+       % Replace either the left or right midpoint depending on the sign of
+       % f(x)
        if fun(x_mid) * fun(x_left) < 0
             x_right = x_mid;
        elseif fun(x_mid) * fun(x_right) < 0
